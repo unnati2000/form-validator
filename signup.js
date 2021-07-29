03eb36f64e4c076aa7542fd96e98a49d68cea250
unnati2000 <unnatibamania8@gmail.com>
2021-07-29 10:33:13 +0530

    If else changes in signup.js

--- signup.js
+++ signup.js
@@ -20,22 +20,24 @@ const formValidation = () => {
   if (email.value === "") {
     email.classList.add("error");
     emailErrorDiv.innerHTML = "Field Empty";
-  } else if (password.value === "") {
+  } else if (!email.value.match(mailformat)) {
+    email.classList.add("error");
+    emailErrorDiv.innerHTML = "Enter a proper email";
+  }
+  if (password.value === "") {
     password.classList.add("error");
     passwordErrorDiv.innerHTML = "Field Empty";
-  } else if (confirmPassword.value === "") {
-    confirmPassword.classList.add("error");
-    confirmPasswordErrorDiv.innerHTML = "Field Empty";
   } else if (password.value.length < 6) {
     password.classList.add("error");
     passwordErrorDiv.innerHTML = "Length of password should be greater than 6";
+  }
+  if (confirmPassword.value === "") {
+    confirmPassword.classList.add("error");
+    confirmPasswordErrorDiv.innerHTML = "Field Empty";
   } else if (password.value !== confirmPassword.value) {
     confirmPassword.classList.add("error");
     confirmPasswordErrorDiv.innerHTML =
       "Password and confirm password should be same";
-  } else if (!email.value.match(mailformat)) {
-    email.classList.add("error");
-    emailErrorDiv.innerHTML = "Enter a proper email";
   } else {
     localStorage.setItem("email", email.value);
     window.location.replace("/");
